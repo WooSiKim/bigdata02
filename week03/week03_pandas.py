52aacc33eed8aaab3489329fcfcdbd6223a36c23
unknown <rlatldn01@email.daelim.ac.kr>
2025-09-11 16:03:33 +0900

    v0.6 pandas

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -1,18 +1,14 @@
-from operator import index
-
-import  pandas as pd
+import pandas as pd
 
 df4 = pd.DataFrame({
-    'date': ['2025-09-11', '2025-09-11', '2025-09-12', '2025-09-12'],
-    'city': ['서울', '안양', '서울', '안양'],
-    'temp': [23, 22, 24, 26]
-}, index=[1, 2, 3, 4])
-
+    'date':['2025-09-11','2025-09-11','2025-09-12','2025-09-12'],
+    'city':['서울', '안양', '서울', '안양'],
+    'temp':[23, 22, 24, 26]},
+    index=[1, 2, 3, 4]
+)
+# print(df4[['date', 'temp']])
+# print(df4['temp'])
 print(df4)
-df9 = df4.sample(n=2) # Randomly select n rows
-print(df9)
-# df10 = df4.nsmallest(2, columns='temp')
-df10 = df4.nlargest(2, columns='temp')
-# print(df10)
-# print(df4.tail(3))
-print(df4.head(3))
+# print(df4.iloc[1:3])
+# print(df4.iloc[:,[0,2]])
+print(df4.iloc[1:3,[0,2]])
