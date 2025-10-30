f39caaaf166132dab34349a5a3bb7b72fb71a6a4
unknown <rlatldn01@email.daelim.ac.kr>
2025-10-30 15:18:17 +0900

    v2.2 pandas

--- week10/week10_pandas.py
+++ week10/week10_pandas.py
@@ -4,8 +4,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 german = pd.read_csv('german.csv')
-#print(german.info())
-#print(german.tail()
-#print(german['Age'].describe())
-#print(pd.cut(german['Age'], bins=10))
-print(pd.cut(german['Age'], bins=8).reset_index().groupby('Age').count())
+# print(german.info())
+# print(german.head())
+# print(german.describe())
+# print(german['Age'].describe())
+# print(pd.cut(german['Age'], bins=8))
+# print(pd.cut(german['Age'], bins=8).reset_index().groupby('Age').count())
+ages = [10, 20, 30, 40, 50, 60, 70, 80]
+# print(pd.cut(german['Age'], bins=ages).reset_index().groupby('Age').count())
+print(pd.cut(german['Age'], bins=ages, right=False).reset_index().groupby('Age').count())
+# print(german[german['Age'] == 20])
